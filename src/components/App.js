0098281c5fc1fbd62d285086ab37eec9c029b3ed
Kshitinjay <kshitinjay20@gmail.com>
2021-02-21 16:13:54 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,81 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+  constructor() {
+    super();
+    this.state = {
+      time: 0,
+      x: 0,
+      y: 0,
+      status: 0,
+    };
   }
+
+  handleIncrement = () => {
+    this.setState({
+      status: 1,
+    });
+    this.clock = setInterval(() => {
+      this.setState({
+        time: this.state.time + 1,
+      });
+    }, 1000);
+  };
+
+  move = (evt) => {
+    if (this.state.status == 1) {
+      console.log(this.state.status);
+      if (evt.keyCode === 37) {
+        this.setState({
+          y: this.state.y - 5,
+        });
+      } else if (evt.keyCode === 38) {
+        this.setState({
+          x: this.state.x - 5,
+        });
+      } else if (evt.keyCode === 39) {
+        this.setState({
+          y: this.state.y + 5,
+        });
+      } else if (evt.keyCode === 40) {
+        this.setState({
+          x: this.state.x + 5,
+        });
+      }
+      if (this.state.x == 250 && this.state.y == 250) {
+        clearInterval(this.clock);
+      }
+    }
+  };
   componentDidMount() {
-    
+    document.addEventListener("keydown", this.move);
+    return () => {
+      document.removeEventListener("keydown", this.move);
+    };
   }
 
   componentWillUnmount() {
-    
+    clearInterval(this.clock);
   }
-
-
 
   render() {
     return (
- <>
-</>
+      <>
+        <div
+          style={{
+            position: "absolute",
+            top: this.state.x,
+            left: this.state.y,
+          }}
+          className="ball"
+        ></div>
+        <button className="start ballProvider" onClick={this.handleIncrement}>
+          Start
+        </button>
+        <div className="heading-timer">{this.state.time}</div>
+
+        <div className="hole"></div>
+      </>
     );
   }
 }
